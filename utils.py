33941a67cbdd8415ae3e321954771861460309bc
nik1806 <nikhil.paliwal14@gmail.com>
2021-07-27 21:14:54 +0200

    Update: change prepro & train sentencepiece model

--- utils.py
+++ utils.py
@@ -5,9 +5,11 @@ from typing import List, Dict
 # functions
 
 def preprocess(text) -> List:
-    file_content = text.lower()
-    file_content = file_content.translate(str.maketrans('', '', string.punctuation))
-    tokens_list = file_content.split()
+    # file_content = text.lower()
+    # file_content = file_content.translate(str.maketrans('', '', string.punctuation))
+    file_content = text
+    tokens_list = file_content.split('\n')
+    tokens_list = [s.strip() for s in tokens_list]
     return tokens_list
 
 def train_test_split_data(text:List, test_size:float=0.1):
