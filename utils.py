eb07c49d4af67234fba58ef119bd0901ac086364
nik1806 <nikhil.paliwal14@gmail.com>
2021-08-02 00:40:38 +0200

    Saving progress: task 4

--- utils.py
+++ utils.py
@@ -1,14 +1,19 @@
 # dependencies
 import string
 from typing import List, Dict
+import nltk.data
 
 # functions
 
 def preprocess(text) -> List:
+    
     # file_content = text.lower()
     # file_content = file_content.translate(str.maketrans('', '', string.punctuation))
     file_content = text
-    tokens_list = file_content.split('\n')
+    # breaking into sentences
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    tokens_list = tokenizer.tokenize(text)
+    # tokens_list = file_content.split('\n')
     tokens_list = [s.strip() for s in tokens_list]
     return tokens_list
 
