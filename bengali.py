d733717197d95969240a051913ef4d85f682ecee
Sangeet Sagar <15uec053@lnmiit.ac.in>
2021-08-03 04:48:58 +0200

    all parts done

--- bengali.py
+++ bengali.py
@@ -19,8 +19,12 @@ import argparse
 import string
 import numpy as np
 from pathlib import Path
-from typing import List, Dict
+import matplotlib.pyplot as plt
+from collections import defaultdict
 from sklearn.model_selection import train_test_split
+from matplotlib import rc
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
 # 1 Data Preparation
 def remove_emoticons(text):
@@ -51,7 +55,7 @@ def data_preparation(text, args, test_size:float=0.2):
             line = remove_emoticons(line) # remove flag symbols
             line = re.sub("[A-Za-z]+","",line) # remove english chars
             line = re.sub(r"\s+", " ", line) # remove white spaces
-            line = re.sub(r'(\W)(?=\1)', '', line)
+            line = re.sub(r'(\W)(?=\1)', '', line) # remove repeated occurrences of punctuation to single occurrence.
             line = line.replace("?", "?\n").replace(",", ",\n").replace("!", "!\n").replace("।", "।\n")
             lines = line.split("\n")
             for line in lines:
@@ -65,6 +69,13 @@ def data_preparation(text, args, test_size:float=0.2):
         train_fpw.write('\n'.join(train))
         test_fpw.write('\n'.join(test))
 
+
+def vocab_generator(data):
+    # pdb.set_trace()
+    vocabs = set(data.split())
+    return list(vocabs)
+
+    
 # def data_preparation(text, args):
 #     """ pre-process text corpus, split into train-test and save"""
 #     all_lines = text.split('\n')
@@ -140,7 +151,7 @@ def lm_train(prefix, class_size, args):
 # 4. Text Generation
 def text_generation(prefix, class_size, args):
     # go from subword unit segmentation back to the original text 
-    os.makedirs('gen_text_bn', exist_ok=True)
+    
     hid = str(args.hid)
     bptt = str(args.bptt)
     ext = prefix + '_hid_' + hid + "_bptt_" + bptt + "_class_" + class_size
@@ -148,22 +159,65 @@ def text_generation(prefix, class_size, args):
     spm_model = str(args.spm_model_dir + '/' + prefix + '_train.model')
     
     for k in range(1, 8):
+        os.makedirs('gen_text_bn/'+ ext, exist_ok=True)
         cmd = "rnnlm/rnnlm \
             -rnnlm " + rnn_model + " \
             -gen " + str(10**k) + " \
             -debug 0 \
-            >> " + str('gen_text_bn/' + ext + '_k_' + str(k) + '.txt')
+            >> " + str('gen_text_bn/' + ext + '/k_' + str(k) + '.txt')
             
         cmd_decode = "spm_decode \
             --model=" + spm_model + " \
             --input_format=piece \
-            < " + str('gen_text_bn/' + ext + '_k_' + str(k) + '.txt') + " \
-            > " + str('gen_text_bn/' + ext + '_k_' + str(k) + '_decoded.txt')
+            < " + str('gen_text_bn/' + ext + '/k_' + str(k) + '.txt') + " \
+            > " + str('gen_text_bn/' + ext + '/k_' + str(k) + '_decoded.txt')
         
         os.system(cmd)
         os.system(cmd_decode)
         
     print("*"*20 + " Text generation completed " + "*"*20)
+    
+def oov_calculator(train, test):
+    unseen_tokens = [tok for tok in test if tok not in train]
+    oov_rate = len(unseen_tokens)/len(test)
+    return oov_rate
+
+# 5. OOV comparison
+def oov_comparison(args):
+    print("*"*10," OOV comparison ","*"*10)
+    train_vocabs = vocab_generator(Path(args.out_dir + "/bn_train.txt").open('r').read())
+    test_vocabs = vocab_generator(Path(args.out_dir + "/bn_test.txt").open('r').read())
+    rnn_vocabs = defaultdict()
+    oov_rates = []
+    for path in Path('gen_text_bn').rglob('*decoded.txt'):
+        k = path.name.split(".")[0]
+        rnn_vocabs[k] = vocab_generator(Path(path).open('r').read())
+        # 5.2
+        print("Computing OOV rate: k=", k.split("_")[1], end = "\r")
+        oov_rates.append(oov_calculator(train_vocabs+rnn_vocabs[k], test_vocabs))
+        
+    all_rnn_vocabs = list(set([word for item in list(rnn_vocabs.values()) for word in item]))
+    # 5.1
+    print("Generating OOV on all vocabs")
+    all_vocabs = train_vocabs + all_rnn_vocabs
+    oov_all_vocabs = oov_calculator(all_vocabs, test_vocabs)
+    print("OOV rate (adding all rnn generated words to train vocab): ", oov_all_vocabs)
+    
+    # 5.3
+    # if error, install 
+    # sudo apt-get install texlive-latex-extra texlive-fonts-recommended dvipng cm-super
+    xticklabels = []
+    xaxis = []
+    for i in range(1,8):
+        xaxis.append(10**i)
+        xticklabels.append(('$10^{'  +str(i) + '}$'))
+    fig, ax = plt.subplots()
+    plt.plot(xaxis, oov_rates)
+    ax.set_xticklabels(xticklabels)
+    ax.set_xlabel(r'\textbf{K}')
+    ax.set_ylabel(r'\textbf{OOV rate}')
+    ax.set_title(r'\textbf{OOV: Bengali corpus}')
+    plt.savefig('bengali_oov.pdf', bbox_inches='tight')
     
 
 def main():
@@ -176,9 +230,11 @@ def main():
     for prefix, vocab_size in enumerate(vocab_sizes):
         class_size = str(args.class_sizes)
         prefix = "bn_s" + str(prefix+1) + "_vocab_size_" + str(vocab_size)
-        model_train(vocab_size, prefix, args)
-        lm_train(prefix, class_size, args)
+        # model_train(vocab_size, prefix, args)
+        # lm_train(prefix, class_size, args)
         # text_generation(prefix, class_size, args)
+        oov_comparison(args)
+        exit()
 
 def parse_arguments():
     """ parse arguments """
