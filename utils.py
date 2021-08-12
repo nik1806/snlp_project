191094c4ece29f8e94b717f8c00f98c569dc4890
nik1806 <nikhil.paliwal14@gmail.com>
2021-08-13 00:16:19 +0200

    update: clean utils

--- utils.py
+++ utils.py
@@ -8,31 +8,21 @@ import numpy as np
 
 def preprocess(text) -> List:
     
-    # file_content = text.lower()
-    # file_content = file_content.translate(str.maketrans('', '', string.punctuation))
     file_content = text
     # breaking into sentences
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
     tokens_list = tokenizer.tokenize(text)
-    # tokens_list = file_content.split('\n')
-    # tokens_list = [s.strip('\n') for s in tokens_list]
     tokens_list = [s.replace('\n', ' ') for s in tokens_list]
     return tokens_list
 
 def train_test_split_data(text:List, test_size:float=0.1):
-    k = int(len(text) * (1 - test_size)) # may be randomize the split later
+    k = int(len(text) * (1 - test_size))
     return text[:k], text[k:]
 
 def vocab_generator(data):
-    # pdb.set_trace()
     data = data.translate(str.maketrans('', '', string.punctuation))
     vocabs = data.split()
     return list(vocabs)
-
-# def oov_calculator(train, test):
-#     unseen_tokens = [tok for tok in test if tok not in train]
-#     oov_rate = len(unseen_tokens)/len(test)
-#     return oov_rate
 
 def oov_calculator(train, test):
     matches = np.in1d(test,train)
